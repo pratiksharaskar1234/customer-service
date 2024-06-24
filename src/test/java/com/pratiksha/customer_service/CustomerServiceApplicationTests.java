f93c8de029a49b4cc398bd6ef1af2d4fc882535e
Pratiksha Raskar <pratiksharaskar1234@gmail.com>
2024-06-24 20:47:26 +0530

    first commit

---
+++ src/test/java/com/pratiksha/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratiksha.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
